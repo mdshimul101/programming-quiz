227e45d34acb5a5e5a279cd79d1a7e61c9803103
mdshimul101 <shimulfjr101@gmail.com>
2022-10-11 03:11:54 +0600

    add topics

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,21 +6,23 @@ const Home = () => {
     const topics = useLoaderData();
     return (
       <div className="w-10/12 m-auto">
-        <div className="sm:flex justify-between w-12/12 lg:w-10/12 md:w-10/12 m-auto mt-5">
+        <div className="md:flex justify-between w-12/12  m-auto mt-5">
           <div>
             <h2 className=" my-6 text-slate-500 text-lg font-bold">
               Hello , Programmer.... <br />
               We are ready to help grow you Programming skill. Participate quiz
-              and develop our programming knowledge
+              and develop our programming knowledge.
             </h2>
           </div>
           <div>
             <img className="w-52" src="favicon.png" alt="" />
           </div>
         </div>
-        {
-            topics.data.map(topic => <Topics key={topic.id} topic={topic}></Topics>)
-        }
+        <div className='grid grid-cols-2 gap-4 mt-20'>
+          {topics.data.map((topic) => (
+            <Topics key={topic.id} topic={topic}></Topics>
+          ))}
+        </div>
       </div>
     );
 };
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -4,7 +4,17 @@ const Topics = ({topic}) => {
     //console.log(topic);
     const {id,name,total,logo} = topic;
     return (
-        <div>
+        <div className=''>
+            <div className='border'>
+                <img className='w-full' src={logo} alt="" />
+                <div className=''>
+                    <h2>{name}</h2>
+                    <p>Total Number of Quiz : {total}</p>
+                </div>
+                <div>
+                    <button className='bg-slate-500 px-4 py-2 w-full '>Practice Quiz</button>
+                </div>
+            </div>
             <h3>{topic.id}</h3>
         </div>
     );
